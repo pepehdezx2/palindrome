36b5f8ad82507c478c8f280f46d4d6157904b18e
Jose Antonio <joseantonio@MacBook-Pro-de-Jose.local>
2017-11-16 10:48:44 -0600

    Initial Commit

---
+++ palindrome/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  palindrome
+//
+//  Created by Jose Antonio on 16/11/17.
+//  Copyright © 2017 Jose Antonio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
