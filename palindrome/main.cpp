fc81c61e5d59d88abe59b046eef7ce6db1fad4bc
José Antonio Hernández <pepehdezx2@gmail.com>
2018-10-30 17:49:03 -0600

    first commit

--- palindrome/main.cpp
+++ palindrome/main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+#include<stdlib.h>
+using namespace std;
+int main() {
+    int dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez, once, doce, mayor;
+    cin >> dos >> tres >> cuatro >> cinco >> seis >> siete >> ocho >> nueve >> diez >> once >> doce;
+    mayor = 0;
+    if (dos>mayor){
+        mayor = dos;
+    }
+    else
+        if (tres>mayor){
+            mayor = tres;
+        }
+        else
+            if (cuatro>mayor){
+                mayor = cuatro;
+            }
+            else
+                if (cinco>mayor){
+                    mayor = cinco;
+                }
+                else
+                    if (seis>mayor){
+                        mayor = seis;
+                    }
+                    else
+                        if (siete>mayor){
+                            mayor = siete;
+                        }
+                        else
+                            if (ocho>mayor){
+                                mayor = ocho;
+                            }
+                            else
+                                if (nueve>mayor){
+                                    mayor = nueve;   
+                                }
+                                else
+                                    if (diez>mayor){
+                                        mayor = diez;   
+                                    }
+                                    else
+                                        if (once>mayor){
+                                            mayor = once;   
+                                        }
+                                        else
+                                            if (doce>mayor){
+                                                mayor = doce;   
+                                            }
     return 0;
 }
